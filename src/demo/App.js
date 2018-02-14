3edc950b90e30d29ce7b117687e003d9d353689c
Kevin Decock <kevin.decock@district01.be>
2018-02-14 20:35:24 +0100

    spaces

--- src/demo/App.js
+++ src/demo/App.js
@@ -2,49 +2,49 @@ import React, { Component } from "react";
 import ComputerKeyboard, { MidiController, MonoSynth, AudioOutput, Distortion, Tremolo, Connector } from "../lib";
 
 class App extends Component {
-	state = {}
+  state = {}
 
-	constructor() {
-	  super();
-	  this.state = {
-	    input: "tremolo",
-	    useMidiController: false,
-	  };
-	}
+  constructor() {
+    super();
+    this.state = {
+      input: "tremolo",
+      useMidiController: false,
+    };
+  }
 
-	render() {
-	  const { useMidiController } = this.state;
+  render() {
+    const { useMidiController } = this.state;
 
-	  return (
-	    <Connector>
-				Toggle between midi and computerKeyboard:
-	      <button onClick={() => this.setState(prevState => ({ useMidiController: !prevState.useMidiController }))}>
-	        {useMidiController ? "use keyboard" : "use midicontroller"}
-	      </button>
+    return (
+      <Connector>
+        Toggle between midi and computerKeyboard:
+        <button onClick={() => this.setState(prevState => ({ useMidiController: !prevState.useMidiController }))}>
+          {useMidiController ? "use keyboard" : "use midicontroller"}
+        </button>
 
 
-				it is possible to route the inputs and outputs however you like:
-	      <button onClick={() => this.setState({ input: "tremolo" })}>{"synth -> tremolo -> output"}</button>
-	      <button onClick={() => this.setState({ input: "distortion" })}>{"synth -> distortion -> output"}</button>
+        it is possible to route the inputs and outputs however you like:
+        <button onClick={() => this.setState({ input: "tremolo" })}>{"synth -> tremolo -> output"}</button>
+        <button onClick={() => this.setState({ input: "distortion" })}>{"synth -> distortion -> output"}</button>
 
-	      {
-	        useMidiController ? (
-	          <MidiController>
-	            {msg => <MonoSynth midiMsg={msg} output={"monosynth"} />}
-	          </MidiController>
-	        ) : (
-	          <ComputerKeyboard controlled keyboardLayout="azerty">
-	            {msg => <MonoSynth midiMsg={msg} output={"monosynth"} />}
-	          </ComputerKeyboard>
-	        )
-	      }
+        {
+          useMidiController ? (
+            <MidiController>
+              {msg => <MonoSynth midiMsg={msg} output={"monosynth"} />}
+            </MidiController>
+          ) : (
+            <ComputerKeyboard controlled keyboardLayout="azerty">
+              {msg => <MonoSynth midiMsg={msg} output={"monosynth"} />}
+            </ComputerKeyboard>
+          )
+        }
 
-	      <Distortion input={"monosynth"} output={"distortion"} />
-	      <Tremolo input={"monosynth"} output={"tremolo"} />
-	      <AudioOutput input={this.state.input} />
-	    </Connector>
-	  );
-	}
+        <Distortion input={"monosynth"} output={"distortion"} />
+        <Tremolo input={"monosynth"} output={"tremolo"} />
+        <AudioOutput input={this.state.input} />
+      </Connector>
+    );
+  }
 }
 
 export default App;

0231654a9fc156cd3c178b1e124e91253f91be18
Kevin Decock <kevin.decock@district01.be>
2018-02-17 13:59:21 +0100

    add enabled/disabled functionality, generalise our effects renderer because its all the same anyway for now

---
+++ src/lib/components/HOC/generateEffectsRenderer.js
@@ -0,0 +1,43 @@
+// a default UI for an effect component.
+import React from "react";
+import lensify from "lensify";
+import { set } from "ramda";
+import { Knob } from "../Atoms";
+
+export default (name, color) => (onChange, params, enabled, onEnableChange) => {
+  const paramLenses = lensify(params);
+  // make sure any edits that happen to the params object is passed through to onChange again
+  // otherwise we lose functionality
+
+  const style = {
+    border: `3px solid ${color}`,
+    padding: 32,
+    margin: 24,
+    display: "inline-block",
+    position: "relative",
+  };
+
+  return (
+    <div style={style}>
+      {name} node!
+      <button
+        style={{ position: "absolute", top: 0, right: 0, margin: 12 }}
+        onClick={() => onEnableChange(!enabled)}
+      >
+        {enabled ? "disable" : "enable"}
+      </button>
+      <div style={{ display: "flex" }}>
+        {Object.keys(params).map(param => (
+          <Knob
+            key={param}
+            onChange={(value) => onChange(set(paramLenses[param], value, params))}
+            value={params[param]}
+            title={param}
+            fgColor={color}
+            style={{ margin: 10 }}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
--- src/lib/components/HOC/index.js
+++ src/lib/components/HOC/index.js
@@ -1,7 +1,9 @@
 import connect from "./connect";
 import generateEffectComponent from "./generateEffectComponent";
+import generateEffectsRenderer from "./generateEffectsRenderer";
 
 export {
   connect,
   generateEffectComponent,
+  generateEffectsRenderer,
 };
